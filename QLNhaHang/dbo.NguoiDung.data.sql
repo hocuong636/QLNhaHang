8b20b3d38e60da7145393bedbc15762a1b4e40fd
duonganh233 <119020433+duonganh233@users.noreply.github.com>
2024-12-22 14:28:54 +0700

    11

---
+++ QLNhaHang/dbo.NguoiDung.data.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[NguoiDung] ON
+INSERT INTO [dbo].[NguoiDung] ([MaNguoiDung], [TenDangNhap], [MatKhau], [HoTen], [Email], [DienThoai], [MaQuyen]) VALUES (1, N'admin', N'280403', N'Duong Trong Tuan Anh', N'duongnguyen280403@gmail.com', N'0353704987', 1)
+INSERT INTO [dbo].[NguoiDung] ([MaNguoiDung], [TenDangNhap], [MatKhau], [HoTen], [Email], [DienThoai], [MaQuyen]) VALUES (2, N'nv002', N'123', N'Nguyen Van A', N'nguyenvana@example.com', N'0123456789', 2)
+INSERT INTO [dbo].[NguoiDung] ([MaNguoiDung], [TenDangNhap], [MatKhau], [HoTen], [Email], [DienThoai], [MaQuyen]) VALUES (3, N'nv003', N'123', N'Tran Thi B', N'tranthib@example.com', N'0123456790', 2)
+INSERT INTO [dbo].[NguoiDung] ([MaNguoiDung], [TenDangNhap], [MatKhau], [HoTen], [Email], [DienThoai], [MaQuyen]) VALUES (4, N'nv004', N'123', N'Pham Thi D', N'phamthid@example.com', N'0123456792', 2)
+INSERT INTO [dbo].[NguoiDung] ([MaNguoiDung], [TenDangNhap], [MatKhau], [HoTen], [Email], [DienThoai], [MaQuyen]) VALUES (5, N'1', N'1', N'Pham Thi D', N'phamthid@example.com', N'0123456792', 1)
+SET IDENTITY_INSERT [dbo].[NguoiDung] OFF
