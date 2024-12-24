e94a40193226e4c973356cb49ad6c34881ff09bd
NguyenThiChi26 <chichixinh2004@gmail.com>
2024-12-24 15:25:44 +0700

    Chi part 2

---
+++ QLNhaHang/QLNhaHang/FoodViewModel.cs/FoodViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace QLNhaHang.ViewModels
+{
+    public class FoodViewModel
+    {
+        // Thuộc tính mô tả món ăn
+        public string Ma { get; set; }
+        public string Ten { get; set; }
+        public decimal Gia { get; set; }
+        public int ThoiGianNau { get; set; }
+
+        // Danh sách các món ăn
+        public ObservableCollection<FoodViewModel> Foods { get; set; }
+
+        public FoodViewModel()
+        {
+            // Dữ liệu mẫu
+            Foods = new ObservableCollection<FoodViewModel>
+            {
+                new FoodViewModel { Ma = "001", Ten = "Phở", Gia = 50000, ThoiGianNau = 15 },
+                new FoodViewModel { Ma = "002", Ten = "Bánh mì", Gia = 15000, ThoiGianNau = 5 },
+                new FoodViewModel { Ma = "003", Ten = "Bánh xèo", Gia = 25000, ThoiGianNau = 10 }
+            };
+        }
+    }
+}
--- QLNhaHang/QLNhaHang/MenuPage/FoodManagementPage.xaml.cs
+++ QLNhaHang/QLNhaHang/MenuPage/FoodManagementPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QLNhaHang.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace QLNhaHang
         public FoodManagementPage()
         {
             InitializeComponent();
+            this.DataContext = new FoodViewModel(); // Thay bằng ViewModel của bạn
         }
     }
 }
